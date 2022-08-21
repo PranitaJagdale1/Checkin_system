1750df5f7f795b198ab4c8b7e94540c7dd85bb0a
PranitaJagdale1 <pranitajagdale9@gmail.com>
2022-08-21 12:52:29 +0530

    First commit

--- src/test/java/com/checkIn/api/FlightCheckinApplicationTests.java
+++ src/test/java/com/checkIn/api/FlightCheckinApplicationTests.java
@@ -1,13 +1,47 @@
 package com.checkIn.api;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.checkIn.api.model.CheckInRecord;
+import com.checkIn.api.repository.CheckInRepository;
 
 @SpringBootTest
 class FlightCheckinApplicationTests {
 
+	@Autowired
+	CheckInRepository cRepo;
+	
+	//to insert record
 	@Test
-	void contextLoads() {
+	public void testcreateCheckin()
+	{
+		CheckInRecord c= new CheckInRecord ();
+		c.setId(1L);
+		c.setLastname("Jagdale");
+		c.setFirstname("Pranita");
+		c.setSeatNumber("6");
+		c.setCheckInTime("2pm");
+		c.setFlightNumber("indigo12");
+		c.setFlightDate("23-9-2022");
+		c.setBookingId(2);
+		cRepo.save(c);
+		assertNotNull(cRepo.findById(1L).get());
+		
+	}
+
+	//to read all records from database
+	@Test
+	public void testgetAllCheckin()
+	{
+		List<CheckInRecord> list = cRepo.findAll();
+		assertThat(list).size().isGreaterThan(0);
 	}
 
 }
