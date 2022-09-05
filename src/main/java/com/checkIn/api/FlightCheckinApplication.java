3e7cb1f1535a702867da81a6c7d9291c3dee0348
PranitaJagdale1 <pranitajagdale9@gmail.com>
2022-09-05 23:03:31 +0530

    First commit

--- src/main/java/com/checkIn/api/FlightCheckinApplication.java
+++ src/main/java/com/checkIn/api/FlightCheckinApplication.java
@@ -1,11 +1,19 @@
 package com.checkIn.api;
 
+import javax.mail.MessagingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.web.client.RestTemplate;
+
+import com.checkIn.api.services.EmailSenderService;
+import javax.mail.MessagingException;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 //@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
@@ -14,6 +22,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class FlightCheckinApplication {
 
+	@Autowired
+	private EmailSenderService senderService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FlightCheckinApplication.class, args);
 	}
@@ -23,5 +34,13 @@ public class FlightCheckinApplication {
 	{
 		return new RestTemplate();
 	}
+	
+	@EventListener(ApplicationReadyEvent.class)
+	public void triggerMail() throws MessagingException
+	{
+		senderService.sendSimpleEmail("pranita.jagdale.it.2018@vpkbiet.org", "Regarding Flight Booking", "Successfully CheckedIn!!!");
+		
+	}
+
 
 }
